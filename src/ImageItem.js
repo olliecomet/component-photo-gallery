eb04608b5c1e0d7a35e2182c8e1cdb4b617cd84f
ollie <leyechi@gmail.com>
2019-08-22 11:19:54 -0700

    deleted comments

--- src/ImageItem.js
+++ src/ImageItem.js
@@ -15,17 +15,3 @@ class ImageItem extends Component {
 }
 
 export default ImageItem;
-
-// below is list item template reference:
-
-// export default function renderImage(photo) {
-//     const html = /*html*/`
-//         <li title="${photo.description}">
-//             <img src="${photo.url}" alt="${photo.title}">
-//             <h3>${photo.title}</h3>
-//             <p>Number of horns: ${photo.horns}</p>
-//         </li>
-//     `;
-
-//     return html;
-// }
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,2 @@
 import './html-equal.js';
-import './render-image.test.js';
 import './ImageItem.test.js';
