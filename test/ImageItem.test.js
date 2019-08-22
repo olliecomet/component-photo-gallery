6b434ae24eb0aafe787aefb831d9f9be131ad723
ollie <leyechi@gmail.com>
2019-08-22 11:17:22 -0700

    passing test

--- test/ImageItem.test.js
+++ test/ImageItem.test.js
@@ -4,9 +4,8 @@ const test = QUnit.test;
 
 QUnit.module('Render image item');
 
-test('', assert => {
+test('renders html from data', assert => {
     // arrange 
-
     const image = {
         url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
         title: 'UniWhal',
@@ -24,8 +23,10 @@ test('', assert => {
     `;
     
     // act
-    
-    
-    // assert
+    const props = { image: image };
+    const imageItem = new ImageItem(props);
+    const html = imageItem.renderHTML();
 
+    // assert
+    assert.htmlEqual(html, expected);
 });
--- test/render-image.test.js
+++ test/render-image.test.js
@@ -1,30 +1,32 @@
-import renderImage from '../src/render-image.js';
+// test from previous lab used for reference
 
-const test = QUnit.test;
+// import renderImage from '../src/render-image.js';
 
-QUnit.module('Render photo item');
+// const test = QUnit.test;
 
-test('renders html from data', assert => {
-    // arrange 
-    const photo = {
-        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
-        title: 'UniWhal',
-        description: 'A unicorn and a narwhal nuzzling their horns',
-        keyword: 'narwhal',
-        horns: 1
-    };
+// QUnit.module('Render photo item');
 
-    const expected = /*html*/`
-        <li title="A unicorn and a narwhal nuzzling their horns">
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="UniWhal">
-            <h3>UniWhal</h3>
-            <p>Number of horns: 1</p>
-        </li>
-    `;
+// test('renders html from data', assert => {
+//     // arrange 
+//     const photo = {
+//         url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+//         title: 'UniWhal',
+//         description: 'A unicorn and a narwhal nuzzling their horns',
+//         keyword: 'narwhal',
+//         horns: 1
+//     };
 
-    // act
-    const html = renderImage(photo);
+//     const expected = /*html*/`
+//         <li title="A unicorn and a narwhal nuzzling their horns">
+//             <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="UniWhal">
+//             <h3>UniWhal</h3>
+//             <p>Number of horns: 1</p>
+//         </li>
+//     `;
 
-    // assert
-    assert.htmlEqual(html, expected);
-});
+//     // act
+//     const html = renderImage(photo);
+
+//     // assert
+//     assert.htmlEqual(html, expected);
+// });
